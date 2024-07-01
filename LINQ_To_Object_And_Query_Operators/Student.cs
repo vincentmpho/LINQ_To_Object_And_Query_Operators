4b3e7632faf1488dcb1c105afea9c9e52afaa15e
Tshindimpho <mudzu13@gmail.com>
2024-07-01 17:37:00 +0200

    Remove Space

--- LINQ_To_Object_And_Query_Operators/Student.cs
+++ LINQ_To_Object_And_Query_Operators/Student.cs
@@ -14,11 +14,9 @@ namespace LINQ_To_Object_And_Query_Operators
         public int Age { get; set; }
 
         //Foereign Key
-
         public int UniversityId { get; set; } 
 
         //Method
-
         public void ShowInfo() 
         {
             Console.WriteLine("Student {0} with id, Gender {2} and Age{3} from university with the Id {4}",
